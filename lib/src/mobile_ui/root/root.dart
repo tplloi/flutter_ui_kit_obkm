d5f20aff73b1ac539ba63f1705ec2bb32268b6c8
loitp <loitp@galaxy.one>
2023-02-09 16:42:36 +0700

    up

--- lib/src/mobile_ui/root/root.dart
+++ lib/src/mobile_ui/root/root.dart
@@ -219,9 +219,8 @@ class RootWidget extends StatelessWidget {
                 page: 37,
                 onPressed: () {
                   context.push("/${MobileRoutes.pg37}");
+                },
               ),
-              
-              
               PageButtonWidget(
                 page: 36,
                 onPressed: () {
